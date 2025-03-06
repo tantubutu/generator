cfe6f7a670f05e98cfbde7821e99b86b5c6f6f09
wangsensen <18137820155@163.com>
2025-03-06 19:50:32 +0800

    静态文件生成尝试

---
+++ wss-generator-basic/src/main/java/com/wss/Main.java
@@ -0,0 +1,16 @@
+package com.wss;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.wss
+ * Description:
+ *
+ * @Author
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
