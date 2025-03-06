923c6a83dfa124a0cfd3def66cb28847831a2288
wangsensen <18137820155@163.com>
2025-03-06 20:02:55 +0800

    静态文件生成(递归复制)

---
+++ wss-generator-basic/src/main/java/com/wss/generator/DynamicGenerator.java
@@ -0,0 +1,13 @@
+package com.wss.generator;
+
+/**
+ * ClassName: DynamicGenerator
+ * Package: com.wss.generator
+ * Description:
+ *
+ * @Author
+ * @Create 2025/3/6 20:01
+ * @Version 1.0
+ */
+public class DynamicGenerator {
+}
---
+++ wss-generator-basic/src/main/java/com/wss/model/MainTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.wss.model;
+
+/**
+ * ClassName: MainTemaplateConfig
+ * Package: com.wss.model
+ * Description:
+ *
+ * @Author
+ * @Create 2025/3/6 19:52
+ * @Version 1.0
+ */
+public class MainTemplateConfig {
+    //在代码开头增加作者 @Author 注释（增加代码）
+    //修改程序输出的信息提示（替换代码）
+    //将循环读取输入改为单次读取（可选代码）
+    public String author;
+    public String outputText;
+    /**
+     * 是否循环
+     */
+    public boolean loop;
+
+}
